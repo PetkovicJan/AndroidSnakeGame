825a51cd33b5ec6eafb04aeac63e59b9705a56ce
Jan <jpetko1@gmail.com>
2021-11-09 23:33:59 +0100

    MainActivity: Make buttons create corresponding activities.

--- app/src/main/java/com/example/snakegame/MainActivity.java
+++ app/src/main/java/com/example/snakegame/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.snakegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button gameButton = (Button)findViewById(R.id.game_button);
+        gameButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent gameIntent = GameActivity.makeIntent(MainActivity.this);
+                startActivity(gameIntent);
+            }
+        });
+
+        Button scoresButton = (Button)findViewById(R.id.score_button);
+        scoresButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent scoresIntent = ScoresActivity.makeIntent(MainActivity.this);
+                startActivity(scoresIntent);
+            }
+        });
     }
 }
