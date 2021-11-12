8f38db4576cf7d582ea78a48230bec5b53e99f9e
Jan <jpetko1@gmail.com>
2021-11-12 14:34:48 +0100

    SnakeGame: Implementation of snake head movement logic.

--- app/src/main/java/com/example/snakegame/SnakeGame.java
+++ app/src/main/java/com/example/snakegame/SnakeGame.java
@@ -1,8 +1,11 @@
 package com.example.snakegame;
 
+import android.graphics.Point;
 import android.util.Log;
 
 public class SnakeGame {
+
+    public enum Direction { UP, DOWN, LEFT, RIGHT }
 
     SnakeGame(int width, int height){
         mWidth = width;
@@ -10,9 +13,8 @@ public class SnakeGame {
     }
 
     public void start(){
-        mStopGame = false;
-
         init();
+        mStopGame = false;
         mThread.start();
     }
 
@@ -20,7 +22,13 @@ public class SnakeGame {
         mStopGame = true;
     }
 
+    public void setDirection(Direction dir){
+        mHeadDir = dir;
+    }
+
     private void init(){
+        mHead = new Point(mWidth/2, mHeight / 2);
+        mHeadDir = Direction.UP;
         mThread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -30,10 +38,31 @@ public class SnakeGame {
     }
     private void mainLoop(){
         while (!mStopGame) {
-            Log.d(TAG, "Game Loop");
+            Log.d(TAG, "Game Loop: " + mHead.toString());
+            update();
             trySleep(500);
         }
+    }
 
+    private void update(){
+        Point newHead = new Point(mHead);
+
+        // Move based on the snake direction.
+        if(mHeadDir == Direction.UP){
+            newHead.y -= HEAD_VEL;
+        } else if(mHeadDir == Direction.DOWN){
+            newHead.y += HEAD_VEL;
+        } else if(mHeadDir == Direction.RIGHT) {
+            newHead.x += HEAD_VEL;
+        } else if(mHeadDir == Direction.LEFT) {
+            newHead.x -= HEAD_VEL;
+        }
+
+        // Wrap the position.
+        newHead.x = wrap(newHead.x, mWidth);
+        newHead.y = wrap(newHead.y, mHeight);
+
+        mHead = newHead;
     }
 
     private void trySleep(int milis){
@@ -44,15 +73,36 @@ public class SnakeGame {
         }
     }
 
+    private int wrap(int num, int max){
+        if(num >= max) {
+            return num - max;
+        }
+        else if(num < 0) {
+            return num + max;
+        }
+        else {
+            return num;
+        }
+    }
+
     // Game size.
-    int mWidth = 0;
-    int mHeight = 0;
+    private int mWidth = 0;
+    private int mHeight = 0;
 
     // Stop game flag.
-    boolean mStopGame = true;
+    private boolean mStopGame = true;
 
     // Thread running main loop.
-    Thread mThread;
+    private Thread mThread;
+
+    // Snake's head position.
+    private Point mHead = null;
+
+    // Snake's head direction.
+    private Direction mHeadDir;
+
+    // Snake's head velocity.
+    private final int HEAD_VEL = 3;
 
     // Debug tag.
     private static final String TAG = "SnakeGame";
