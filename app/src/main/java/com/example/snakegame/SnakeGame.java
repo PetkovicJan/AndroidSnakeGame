418163e2755c6b2b5db51cf6a16b9e13240df2e6
Jan <jpetko1@gmail.com>
2021-11-12 08:29:24 +0100

    SnakeGame: Addition of a class, that will implement the game logic.

---
+++ app/src/main/java/com/example/snakegame/SnakeGame.java
@@ -0,0 +1,12 @@
+package com.example.snakegame;
+
+public class SnakeGame {
+
+    SnakeGame(int width, int height){
+        mWidth = width;
+        mHeight = height;
+    }
+
+    int mWidth = 0;
+    int mHeight = 0;
+}
