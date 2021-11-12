5aa10d1001781340efbb2e4b05841b543a03399b
Jan <jpetko1@gmail.com>
2021-11-12 07:51:11 +0100

    GameActivity: Addition of new GameView to its placeholder.

--- app/src/main/java/com/example/snakegame/GameActivity.java
+++ app/src/main/java/com/example/snakegame/GameActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -18,5 +19,12 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // Add game view.
+        LinearLayout container = (LinearLayout)findViewById(R.id.game_view);
+        mGameView = new GameView(this, 800, 800);
+        container.addView(mGameView);
     }
+
+    GameView mGameView;
 }
