56054ee869216b40c4593c429af28901517ee336
Jan <jpetko1@gmail.com>
2021-11-12 08:59:11 +0100

    SnakeGame: Addition of main loop, which runs in a separate thread.

--- app/src/main/java/com/example/snakegame/SnakeGame.java
+++ app/src/main/java/com/example/snakegame/SnakeGame.java
@@ -1,5 +1,7 @@
 package com.example.snakegame;
 
+import android.util.Log;
+
 public class SnakeGame {
 
     SnakeGame(int width, int height){
@@ -7,6 +9,51 @@ public class SnakeGame {
         mHeight = height;
     }
 
+    public void start(){
+        mStopGame = false;
+
+        init();
+        mThread.start();
+    }
+
+    public void stop(){
+        mStopGame = true;
+    }
+
+    private void init(){
+        mThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mainLoop();
+            }
+        });
+    }
+    private void mainLoop(){
+        while (!mStopGame) {
+            Log.d(TAG, "Game Loop");
+            trySleep(500);
+        }
+
+    }
+
+    private void trySleep(int milis){
+        try {
+            Thread.sleep(milis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Game size.
     int mWidth = 0;
     int mHeight = 0;
+
+    // Stop game flag.
+    boolean mStopGame = true;
+
+    // Thread running main loop.
+    Thread mThread;
+
+    // Debug tag.
+    private static final String TAG = "SnakeGame";
 }
